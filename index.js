466363c5a3e582f6b7304aeeff79912358167ba4
M. Y. Katz <mykatz01@gmail.com>
2019-06-13 19:40:04 -0700

    Code added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //index.js
+
+module.exports = function() {
+    return function(req, res, next) {
+        saveData = req.get("Save-Data");
+        if(saveData){
+            req.dataSaver = true;
+            req.saveData = req.dataSaver;
+        }
+        else{
+            req.dataSaver = false;
+            req.saveData = req.dataSaver;
+        }
+        next()
+    }
+}
